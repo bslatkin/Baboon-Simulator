9138bd2500746634f327f7bc31f619f3e102e3c7
Brett Slatkin <bslatkin@gmail.com>
2011-12-27 16:07:59 -0800

    really this time

--- baboon.go
+++ baboon.go
@@ -4,20 +4,46 @@ import "fmt"
 
 
 type Color string
+type Position string
 
 const (
-	RED Color = "red"
-	BLUE Color = "blue"
+	red Color = "red"
+	blue Color = "blue"
+
+	rope Position = "rope"
+	left Position = "left"
+	right Position = "right"
 )
 
 
 type Baboon struct {
-	number int
+	id int
 	color Color
+	pos Position
+}
+
+
+type Rope struct {
+	id int
 }
 
 
 func main() {
-	b := Baboon{2, RED}
-	fmt.Printf("Hello world %#v\n", b)
+	baboons := []*Baboon{}
+	for i := 0; i < 100; i++ {
+		c := red
+		p := left
+		if i % 2 == 0 {
+			c = blue
+			p = right
+		}
+		baboons = append(baboons, &Baboon{id: i, color: c, pos: p})
+	}
+
+	ropes := []*Rope{}
+	for i := 0; i < 10; i++ {
+		ropes = append(ropes, &Rope{id: i})
+	}
+
+	fmt.Printf("Hello world %#v, %#v\n", baboons, ropes)
 }
