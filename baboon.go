1ba4290b12297f23a4be771a448bb6fc0c5cdea3
Brad Fitzpatrick <brad@danga.com>
2011-12-27 16:14:20 -0800

    some goroutines

--- baboon.go
+++ baboon.go
@@ -1,49 +1,79 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"time"
+)
 
+var _ = log.Printf
 
 type Color string
 type Position string
 
 const (
-	red Color = "red"
+	red  Color = "red"
 	blue Color = "blue"
 
-	rope Position = "rope"
-	left Position = "left"
+	rope  Position = "rope"
+	left  Position = "left"
 	right Position = "right"
 )
 
-
 type Baboon struct {
-	id int
+	id    int
 	color Color
-	pos Position
+	pos   Position
 }
-
 
 type Rope struct {
 	id int
 }
-
 
 func main() {
 	baboons := []*Baboon{}
 	for i := 0; i < 100; i++ {
 		c := red
 		p := left
-		if i % 2 == 0 {
+		if i%2 == 0 {
 			c = blue
 			p = right
 		}
-		baboons = append(baboons, &Baboon{id: i, color: c, pos: p})
+		b := &Baboon{id: i, color: c, pos: p}
+		baboons = append(baboons, b)
+		go b.live()
 	}
 
 	ropes := []*Rope{}
 	for i := 0; i < 10; i++ {
-		ropes = append(ropes, &Rope{id: i})
+		r := &Rope{id: i}
+		go r.hang()
+		ropes = append(ropes, r)
 	}
 
 	fmt.Printf("Hello world %#v, %#v\n", baboons, ropes)
+	select {}
+}
+
+// The baboon's lifecycle.
+func (b *Baboon) live() {
+	for {
+		select {}
+	}
+}
+
+// The riope's lifecycle.
+func (r *Rope) hang() {
+	tick := time.NewTicker(100 * time.Millisecond)
+	for {
+		select {
+		case <-tick.C:
+			r.moveBaboons()
+		}
+	}
+}
+
+func (r *Rope) moveBaboons() {
+	log.Printf("moving baboons on rope %d", r.id)
+	
 }
